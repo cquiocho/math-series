dddc5195b2becab9e981f023156eaabf372e5621
Christopher Quiocho <christopher.quiocho@gmail.com>
2020-11-10 15:50:19 -0800

    Lucas Numbers Complete

--- tests/test_math_series.py
+++ tests/test_math_series.py
@@ -24,3 +24,23 @@ def test_fibonacci_3():
     actual = fibonacci(3)
     expected = 2
     assert actual == expected
+
+def test_lucas_0():
+    actual = lucas(0)
+    expected = 2
+    assert actual == expected
+
+def test_lucas_1():
+    actual = lucas(1)
+    expected = 1
+    assert actual == expected
+
+def test_lucas_2():
+    actual = lucas(2)
+    expected = 3
+    assert actual == expected
+
+def test_lucas_3():
+    actual = lucas(3)
+    expected = 4
+    assert actual == expected
