6028610662e6c8d2df5656d1c0a9bfc16439b53a
Christopher Quiocho <christopher.quiocho@gmail.com>
2020-11-10 16:13:21 -0800

    Sum_Series Sequence Complete

--- math_series/math_series.py
+++ math_series/math_series.py
@@ -24,3 +24,14 @@ def lucas(n):
 
   else:
    return lucas(n - 1) + lucas(n - 2)
+
+
+def sum_series(n, optionA = 0, optionB = 1):
+  if n == 0:
+    return optionA
+
+  if n == 1:
+    return optionB
+
+  else:
+   return sum_series(n - 1, optionA, optionB) + sum_series(n - 2, optionA, optionB)
--- tests/test_math_series.py
+++ tests/test_math_series.py
@@ -1,9 +1,10 @@
-from math_series.math_series import fibonacci, lucas
+from math_series.math_series import fibonacci, lucas, sum_series
 
 
 def test_import():
     assert fibonacci
     assert lucas
+    assert sum_series
 
 def test_fibonacci_0():
     actual = fibonacci(0)
@@ -43,4 +44,29 @@ def test_lucas_2():
 def test_lucas_3():
     actual = lucas(3)
     expected = 4
+    assert actual == expected
+
+def test_sum_series_0():
+    actual = sum_series(0)
+    expected = 0
+    assert actual == expected
+
+def test_sum_series_1():
+    actual = sum_series(1)
+    expected = 1
+    assert actual == expected
+
+def test_sum_series_2():
+    actual = sum_series(2)
+    expected = 1
+    assert actual == expected
+
+def test_sum_series_3():
+    actual = sum_series(3)
+    expected = 2
+    assert actual == expected
+
+def test_sum_series_4():
+    actual = sum_series(4)
+    expected = 3
     assert actual == expected
